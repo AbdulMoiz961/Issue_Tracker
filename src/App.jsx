4813f98a0a9bcc5e71d6c2d4763a07794be1f776
QewOffender3k <94215329+AbdulMoiz961@users.noreply.github.com>
2024-02-29 12:50:54 +0500

    Adding input validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-const initialIssues = [
-    {
-        id: 1,
-        status: 'New',
-        owner: 'Ravan', 
-        effort: 5,
-        created: new Date('2018-08-15'),
-        due: undefined,
-        title: 'Error in console when clicking add',
-    },
-    {
-        id: 2,
-        status: 'Assigned',
-        owner: 'Eddie', 
-        effort: 14,
-        created: new Date('2018-08-16'),
-        due: new Date('2018-08-30'),
-        title: 'Missing bottom border on panel',
-    }
-];
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+    if (dateRegex.test(value)) return new Date(value);
+    return value;
+}
 
 class IssueFilter extends React.Component {
     render() {
@@ -79,7 +65,7 @@ class IssueAdd extends React.Component {
         const issue = {
             owner: form.owner.value,
             title: form.title.value,
-            status: 'New',
+            due: new Date(new Date().getTime() + 1000*60*60*24*10),
         }
         this.props.createIssue(issue);
         form.owner.value = "";
@@ -97,6 +83,31 @@ class IssueAdd extends React.Component {
     }
 }
 
+async function graphQLFetch(query, variables = {}) {
+    try {
+        const response = await fetch('graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query, variables })
+        });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors) {
+            const error = result.errors[0];
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`);
+            }
+        }
+        return result.data;
+    } catch (e) {
+        alert(`Error in sending data to server: ${e.message}`);
+    }
+}
+
 class IssueList extends React.Component {
     constructor() {
         super();
@@ -108,18 +119,31 @@ class IssueList extends React.Component {
         this.loadData();
     }
 
-    loadData() {
-        setTimeout(() => {
-            this.setState({ issues: initialIssues });
-        }, 500);
+    async loadData() {
+        const query = `query {
+            issueList {
+                id title status owner
+                created effort due
+            }
+        }`;
+
+        const data = await graphQLFetch(query);
+        if (data) {
+            this.setState({ issues: data.issueList });
+        }
     }
 
-    createIssue(issue) {
-        issue.id = this.state.issues.length + 1;
-        issue.created = new Date();
-        const newIssueList = this.state.issues.slice();
-        newIssueList.push(issue);
-        this.setState({ issues: newIssueList });
+    async createIssue(issue) {
+        const query = `mutation issueAdd($issue: IssueInputs!) {
+            issueAdd(issue: $issue) {
+                id
+            }
+        }`
+
+        const data = await graphQLFetch(query, { issue });
+        if (data) {
+            this.loadData();
+        }
     }
 
     render() {
