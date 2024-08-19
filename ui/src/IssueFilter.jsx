24fae100bfa17c84286fd92e7b0862d06354d545
Abdul Moiz <shabdulmoiz96@gmail.com>
2024-08-19 23:19:15 +0500

    Front-End Modularization and Webpack

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint "react/prefer-stateless-function": "off" */
+
+import React from 'react';
+
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
